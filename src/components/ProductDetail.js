00f7457009599f259c00d95db8fba8c1e3cd96b8
Karl Mark Balagtey <karl.balagtey@gmail.com>
2018-06-20 21:56:14 +0100

    Added card, card section, updated product detail

--- src/components/ProductDetail.js
+++ src/components/ProductDetail.js
@@ -1,12 +1,27 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import Card from './Card';
+import CardSection from './CardSection';
 
 const ProductDetail = (props) => {
 	return (
-		<View>
-			<Text>{props.product.title}</Text>
-		</View>
+		<Card>
+			<CardSection>
+				<View></View>
+				<View style={styles.headerContentStyle}>
+					<Text>{props.product.title}</Text>
+					<Text>{props.product.details}</Text>
+				</View>
+			</CardSection>
+		</Card>
 	);
 };
+
+const styles = {
+	headerContentStyle: {
+		flexDirection: 'column',
+		justifyContent: 'space-around'
+	}
+}
 
 export default ProductDetail;
